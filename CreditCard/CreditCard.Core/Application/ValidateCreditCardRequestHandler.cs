339d474230ec6b7ec19097eb2f4c1ae4d088052f
Sm-Mosavian <senexen@gmail.com>
2024-11-17 10:58:53 +1300

    Add ValidateCreditCardRequestHandler class to Core project and its unittest to Test project

---
+++ CreditCard/CreditCard.Core/Application/ValidateCreditCardRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace CreditCard.Core.Application
+{
+    public class ValidateCreditCardRequestHandler : IRequestHandler<ValidateCreditCardRequest, ErrorOr<bool>>
+    {
+
+        public async Task<ErrorOr<bool>> Handle(ValidateCreditCardRequest request, CancellationToken cancellationToken)
+        {
+            var result = BeValidLuhn(request.CardNumber);
+            return await Task.FromResult(result);
+        }
+        private bool BeValidLuhn(string cardNumber)
+        {
+            int sum = 0;
+            bool alternate = false;
+            for (int i = cardNumber.Length - 1; i >= 0; i--)
+            {
+                int n = int.Parse(cardNumber[i].ToString());
+                if (alternate)
+                {
+                    n *= 2;
+                    if (n > 9) n -= 9;
+                }
+                sum += n;
+                alternate = !alternate;
+            }
+            return sum % 10 == 0;
+        }
+    }
+}
---
+++ CreditCard/CreditCard.Test/UnitTests/ValidateCreditCardRequestHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using CreditCardValidator.Core.Application;
+using ErrorOr;
+
+namespace CreditCard.Test.UnitTests
+{
+    public class ValidateCreditCardRequestHandlerTests
+    {
+        private readonly ValidateCreditCardRequestHandler _handler;
+
+        public ValidateCreditCardRequestHandlerTests()
+        {
+            _handler = new ValidateCreditCardRequestHandler();
+        }
+
+        [Theory]
+        [InlineData("4111111111111111", true)]
+        [InlineData("5500000000000004", true)]
+        [InlineData("1234567812345670", true)]
+        [InlineData("6011000000000004", true)]
+        [InlineData("4111111111111120", false)]
+        [InlineData("5500000000000005", false)]
+        [InlineData("6011000000000003", false)]
+        public async Task Handle_Should_Return_Expected_Result(string cardNumber, bool expected)
+        {
+            // Arrange
+            var request = new ValidateCreditCardRequest(cardNumber);
+
+            // Act
+            ErrorOr<bool> result = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.Equal(expected, result.Value);
+        }
+    }
+
+
+}
